b88592d9b9ee6e5a685fdfd7a41d93725e23dc57
LaraFriedrichs <152259928+LaraFriedrichs@users.noreply.github.com>
2024-09-20 07:55:12 +0000

    test

--- test.py
+++ test.py
@@ -1,8 +1,27 @@
 import streamlit as st
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import plotly
 
 st.title('Data Science Short Course')
 st.write('*This is a test for a streamlit App*')
 
-df = pd.read_csv()
+df = pd.read_csv("data/Banda Arc.csv")
+
+cat_names = df.columns.tolist()[27:]
+
+#st.write(cat_names)
+
+st.multiselect('Chose coloumns',options=cat_names)
+
+col1,col2 = st.columns(2)
+
+with col1:
+    el1 = st.selectbox('x-axis',cat_names)
+    el2 = st.selectbox('y-axis', cat_names)
+with col2:
+    fig = plt.figure()
+    plt.scatter(df[el1]/1000,df[el2]/1000)
+    st.pyplot(fig)
+
+st.dataframe(df)
