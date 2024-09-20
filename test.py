55f85c84a550fa24cb964208a47c8cc52cec548b
LaraFriedrichs <152259928+LaraFriedrichs@users.noreply.github.com>
2024-09-20 07:10:46 +0000

    c1

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 st.title('Data Science Short Course')
+st.write('*This is a test for a streamlit App*')
+
+df = pd.read_csv()
